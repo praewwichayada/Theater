ae59ccd94f15701a9ade995ff5b99fb63f42d8df
WichayadaH <wichayada.h@ku.th>
2021-11-08 01:27:18 +0700

    created OAuth Login

--- src/main/java/theater/controller/HomeController.java
+++ src/main/java/theater/controller/HomeController.java
@@ -1,6 +1,8 @@
 package theater.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,7 @@ public class HomeController {
     MovieService movieService;
 
     @RequestMapping("/")
-    public String getHomePage(Model model){
+    public String getHomePage(Model model, @AuthenticationPrincipal OAuth2User principal){
         List<Movie> movies = movieService.getALl();
         List<Movie> moviesOnAir = new ArrayList<>();
         List<Movie> moviesComingSoon = new ArrayList<>();
@@ -31,6 +33,13 @@ public class HomeController {
 
         model.addAttribute("moviesOnAir", moviesOnAir);
         model.addAttribute("moviesComingSoon", moviesComingSoon);
+
+
+        if (principal != null)
+            model.addAttribute("user", principal.getAttribute("email"));
+        else
+            model.addAttribute("user", "Guest");
+
         return "home";
     }
 }
