09e3665892f7606a11dbc9ab08b296f3273901ef
waroonsuppakit <waroon.s@ku.th>
2021-11-09 05:59:59 +0700

    Show information in show_time page

--- src/main/java/theater/controller/MovieShowtimeController.java
+++ src/main/java/theater/controller/MovieShowtimeController.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import theater.model.Cinema;
+import theater.model.ShowTime;
+import theater.service.CinemaService;
 import theater.service.MovieService;
 import theater.service.PlaceService;
 import theater.service.ShowTimeService;
 
-import java.util.UUID;
+import java.util.*;
 
 @Controller
 public class MovieShowtimeController {
@@ -20,13 +23,36 @@ public class MovieShowtimeController {
     PlaceService placeService;
 
     @Autowired
+    CinemaService cinemaService;
+
+    @Autowired
     ShowTimeService showTimeService;
 
     @RequestMapping("movies/{id}")
     public String getMovieShowtimePage(@PathVariable UUID id,Model model){
         model.addAttribute("movie", movieService.getMovie(id));
+
+        List<Cinema> cinemaList = cinemaService.getALl();
+        Collections.sort(cinemaList, new Comparator<Cinema>() {
+            @Override
+            public int compare(Cinema o1, Cinema o2) {
+                return o1.getNumber().compareTo(o2.getNumber());
+            }
+        });
+        model.addAttribute("cinemas", cinemaList);
+
         model.addAttribute("places", placeService.getALl());
-        model.addAttribute("showtimes", showTimeService.getALl());
+
+
+        List<ShowTime> showTimeList = showTimeService.getALl();
+        Collections.sort(showTimeList, new Comparator<ShowTime>() {
+            @Override
+            public int compare(ShowTime o1, ShowTime o2) {
+                return o1.getDateTime().compareTo(o2.getDateTime());
+            }
+        });
+        model.addAttribute("showtimes", showTimeList);
+
         return "movieshowtime";
     }
 
