3a08201a2cd3b1c0ea1b2480859c650e6e657418
waroonsuppakit <waroon.s@ku.th>
2021-11-09 19:11:45 +0700

    Update add information into all theater page

--- src/main/java/theater/controller/NormalTheaterController.java
+++ src/main/java/theater/controller/NormalTheaterController.java
@@ -1,12 +1,37 @@
 package theater.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import theater.service.CinemaService;
+import theater.service.MovieService;
+import theater.service.PlaceService;
+import theater.service.ShowTimeService;
+
+import java.util.UUID;
 
 @Controller
 public class NormalTheaterController {
-    @RequestMapping("/normaltheater")
-    public String getThreeDPage() {
+    @Autowired
+    MovieService movieService;
+
+    @Autowired
+    PlaceService placeService;
+
+    @Autowired
+    CinemaService cinemaService;
+
+    @Autowired
+    ShowTimeService showTimeService;
+
+    @RequestMapping("/normaltheater/{movieID}/{placeID}/{cinemaID}/{showTimeID}")
+    public String getThreeDPage(@PathVariable UUID movieID, @PathVariable UUID placeID, @PathVariable UUID cinemaID, @PathVariable UUID showTimeID, Model model) {
+        model.addAttribute("movie", movieService.getMovie(movieID));
+        model.addAttribute("place", placeService.getPlace(placeID));
+        model.addAttribute("cinema", cinemaService.getCinema(cinemaID));
+        model.addAttribute("showTime", showTimeService.getShowTime(showTimeID));
         return "normaltheater";
     }
 }
