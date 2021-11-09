05808edfaeb9de4334a87b7a4e1305e52ca95a22
plengnelq42 <sonsawan.ko@ku.th>
2021-11-09 16:43:02 +0700

    edit Booking data

---
+++ src/main/java/theater/controller/NormalTheaterController.java
@@ -0,0 +1,12 @@
+package theater.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NormalTheaterController {
+    @RequestMapping("/normaltheater")
+    public String getThreeDPage() {
+        return "normaltheater";
+    }
+}
